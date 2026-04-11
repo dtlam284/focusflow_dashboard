5646b42d4abb8fb89ff20fd524b9f0cd0099ea25
dltam04 <zinmrx04@gmail.com>
2026-04-11 16:16:34 +0700

    Task 3: import path change

--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -1,8 +1,8 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import { persistReducer, persistStore } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
-import authReducer from '../../features/auth/store/slices/authSlice'
-import appReducer from '../../app/store/slices/appSlice'
+import authReducer from '@/features/auth/store/slices/authSlice'
+import appReducer from '@/app/store/slices/appSlice'
 
 const persistConfig = {
   key: 'cms',
